a020826c047c4ad65b294e00d439fd5f2e1babcf
Nickolay Artyushkin <NickolayA@humanforce.com>
2020-09-03 21:52:23 +0300

    1

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 export const LinkCard = ({ link }) => {
     const shortLink = window.location.origin.toString() + '/t/' + link.code;
+    console.log(shortLink);
+    console.log(link);
     return (
         <div>
             <h2>Link</h2>
-            <p>Short link: <a href={shortLink} target="_blank" rel="noopener noreferrer">{shortLink}</a></p>
+            <p>Short link: <a href={window.location.origin.toString() + '/t/' + link.code} target="_blank" rel="noopener noreferrer">{shortLink}</a></p>
             <p>Original link: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
             <p>Clicks: <strong>{link.clicks}</strong></p>
             <p>Creation date: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
