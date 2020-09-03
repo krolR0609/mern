f0caee3f405044f045370db03c2ffcc4d79c78b8
Nickolay Artyushkin <NickolayA@humanforce.com>
2020-09-03 22:10:13 +0300

    Ds

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -4,7 +4,7 @@ export const LinkCard = ({ link }) => {
     return (
         <div>
             <h2>Link</h2>
-            <p>Short link: <a href={link.code} target="_blank" rel="noopener noreferrer">{link.code}</a></p>
+            <p>Short link: <a href={link.code} target="_blank" rel="noopener noreferrer">Привет:{link.code}</a></p>
             <p>Original link: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
             <p>Clicks: <strong>{link.clicks}</strong></p>
             <p>Creation date: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
